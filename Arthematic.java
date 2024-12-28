3e8c3d8c0c72e0b4d6616ec3e4e5f06068933591
Rajashekar <edulapuramrajashekar91@gmail.com>
2024-12-28 06:38:08 +0530

    firstone

---
+++ Arthematic.java
@@ -0,0 +1,24 @@
+package first;
+
+public class Arthematic {
+	
+	public static void main(String[] args) {
+		int a=20;
+		int b=15;
+		int add,sub,div,mod,mul;
+		add=a+b;
+		sub=a-b;
+		div=a/b;
+		mod=a/b;
+		mul=a*b;
+		System.out.println("The value of a "+a);
+		System.out.println("The value of b "+b);
+	
+		System.out.println("The value of Addition "+add);
+		System.out.println("The value of Subtraction "+sub);
+		System.out.println("The value of Divison "+div);
+		System.out.println("The value of Modulus "+mod);
+		System.out.println("The value of Multiplication "+mul);
+	}
+
+}
